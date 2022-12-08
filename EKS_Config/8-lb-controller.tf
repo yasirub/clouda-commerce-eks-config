972d667947b2d483ea47ef10976f3396b0ac092a
yasirub <yasiru.bhagya@clouda.io>
2022-12-08 08:33:46 +0530

    adds lb-controller installation

---
+++ EKS_Config/8-lb-controller.tf
@@ -0,0 +1,58 @@
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.clouda-commerce-cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.clouda-commerce-cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.clouda-commerce-cluster.id]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "aws-load-balancer-controller" {
+  name = "aws-load-balancer-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.4.1"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.clouda-commerce-cluster.id
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.4.2"
+  }
+
+  set {
+    name  = "replicaCount"
+    value = 1
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.aws_load_balancer_controller.arn
+  }
+
+  # EKS Fargate specific
+  set {
+    name  = "region"
+    value = "us-east-1"
+  }
+
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
+  depends_on = [aws_eks_fargate_profile.kube-system]
+
+}
